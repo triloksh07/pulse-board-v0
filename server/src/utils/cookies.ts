e84706b4b47b2a2e99da74bc9073a932ada62d15
triloksh07 <trilok0726@gmail.com>
2026-05-16 00:06:27 +0530

    fix(cors)

--- server/src/utils/cookies.ts
+++ server/src/utils/cookies.ts
@@ -8,7 +8,7 @@ export function setAuthCookie(res: Response, token: string): void {
   res.cookie("pulseboard_token", token, {
     httpOnly: true,
     secure: isProduction && !isLocal,
-    sameSite: isProduction && !isLocal ? "strict" : "lax",
+    sameSite: isProduction && !isLocal ? "none" : "lax",
     maxAge: 1000 * 60 * 60 * 24 * 7,
   });
 }
@@ -17,6 +17,6 @@ export function clearAuthCookie(res: Response): void {
   res.clearCookie("pulseboard_token", {
     httpOnly: true,
     secure: isProduction,
-    sameSite: isProduction ? "strict" : "lax",
+    sameSite: isProduction ? "none" : "lax",
   });
 }
